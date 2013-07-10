94ac8c9553c93c4c8d5ac111576dc6fc2fa34927
John Bragg <remotezygote@gmail.com>
2013-07-10 15:52:41 -0700

    works

--- lib/slack/post.rb
+++ lib/slack/post.rb
@@ -1,7 +1,74 @@
 require "slack/post/version"
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'yajl'
 
 module Slack
-  module Post
-    # Your code goes here...
-  end
+	module Post
+		
+		DefaultOpts = {
+			channel: '#general'
+		}.freeze
+		
+		def self.post(message,chan=nil)
+			raise "You need to call Slack::Post.configure before trying to send messages." unless configured?(chan.nil?)
+			pkt = {
+				channel: chan || config[:channel],
+				text: message,
+			}
+			if config[:username]
+				pkt[:username] = config[:username]
+			end
+			uri = URI.parse(post_url)
+			http = Net::HTTP.new(uri.host, uri.port)
+			http.use_ssl = true
+			http.ssl_version = :SSLv3
+			http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+			req = Net::HTTP::Post.new(uri.request_uri)
+			req.body = Yajl::Encoder.encode(pkt)
+			req["Content-Type"] = 'application/json'
+			resp = http.request(req)
+			case resp
+				when Net::HTTPSuccess
+					return true
+				else
+					raise "There was an error while trying to post. Error was: #{resp.body}"
+			end
+		end
+		
+		def self.post_url
+			"https://#{config[:subdomain]}.slack.com/services/hooks/incoming-webhook?token=#{config[:token]}"
+		end
+		
+		NecessaryConfigParams = [:subdomain,:token].freeze
+		
+		def self.configured?(needs_channel=true)
+			return false if needs_channel and !config[:channel]
+			NecessaryConfigParams.all? do |parm|
+				config[parm]
+			end
+		end
+		
+		def self.config
+			@config ||= DefaultOpts
+		end
+		
+		def self.configure(opts)
+			@config = config.merge(prune(opts))
+		end
+		
+		KnownConfigParams = [:username,:channel,:subdomain,:token].freeze
+		
+		def self.prune(opts)
+			opts.inject({}) do |acc,(k,v)|
+				k = k.to_sym
+				if KnownConfigParams.include?(k)
+					acc[k] = v
+				end
+				acc
+			end
+		end
+		
+	end
 end
--- slack-post.gemspec
+++ slack-post.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = Slack::Post::VERSION
   spec.authors       = ["John Bragg"]
   spec.email         = ["remotezygote@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
+  spec.description   = %q{Pretty simply really. It posts to slack fer ya.}
+  spec.summary       = %q{It's for posting messages to your slack.}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+	spec.add_dependency 'yajl-ruby'
 end
